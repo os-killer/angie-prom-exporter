fb415657aca7ae631b5717f69b142c44e429a16d
OS_Killer <47445476+os-killer@users.noreply.github.com>
2022-11-22 14:09:52 +0300

    Update install_n_run.sh

--- install_n_run.sh
+++ install_n_run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-sudo -i
-apt-get update && \
-apt-get install --no-install-recommends --no-install-suggests --yes \
+sudo apt-get update && \
+sudo apt-get install --no-install-recommends --no-install-suggests --yes \
                     ca-certificates curl gnupg lsb-release
 
-mkdir -p /etc/apt/keyrings && \
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings && \
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update && \
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update && \
+sudo apt-get install --no-install-recommends --no-install-suggests --yes \
+                    docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-docker compose up --build
+sudo docker compose up --build
